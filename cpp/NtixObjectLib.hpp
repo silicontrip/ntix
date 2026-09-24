8008defb673f4b2c7507bde812e2d1a529d82d08
mheath <mheath@nine.com.au>
2026-09-24 16:47:44 +1000

    Added more Object functions

--- cpp/NtixObjectLib.hpp
+++ cpp/NtixObjectLib.hpp
@@ -14,13 +14,28 @@ typedef struct _OBJECT_DIRECTORY_INFORMATION
 } OBJECT_DIRECTORY_INFORMATION, *POBJECT_DIRECTORY_INFORMATION;
 
 #define NTDLL_OBJECT_EXPORTS \
+	X(NtAdjustPrivilegesToken, NTSTATUS, (HANDLE, BOOLEAN, PTOKEN_PRIVILEGES, ULONG, PTOKEN_PRIVILEGES, PULONG)) \
 	X(NtClose, NTSTATUS, (HANDLE)) \
-	X(NtOpenSymbolicLinkObject, NTSTATUS, (PHANDLE, ACCESS_MASK, POBJECT_ATTRIBUTES)) \
-	X(NtQuerySymbolicLinkObject, NTSTATUS, (HANDLE, PUNICODE_STRING, PULONG)) \
+	X(NtMakeTemporaryObject, NTSTATUS, (HANDLE)) \
 	X(NtOpenDirectoryObject, NTSTATUS, (PHANDLE, ACCESS_MASK, POBJECT_ATTRIBUTES)) \
+	X(NtOpenEvent, NTSTATUS, (PHANDLE, ACCESS_MASK, POBJECT_ATTRIBUTES)) \
+	X(NtOpenMutant, NTSTATUS, (PHANDLE, ACCESS_MASK, POBJECT_ATTRIBUTES)) \
+	X(NtOpenProcessToken, NTSTATUS, (HANDLE, ACCESS_MASK, PHANDLE)) \
+	X(NtOpenSection, NTSTATUS, (PHANDLE, ACCESS_MASK, POBJECT_ATTRIBUTES)) \
+	X(NtOpenSemaphore, NTSTATUS, (PHANDLE, ACCESS_MASK, POBJECT_ATTRIBUTES)) \
+	X(NtOpenSymbolicLinkObject, NTSTATUS, (PHANDLE, ACCESS_MASK, POBJECT_ATTRIBUTES)) \
+	X(NtPrivilegeCheck, NTSTATUS, (HANDLE, PPRIVILEGE_SET, PBOOLEAN)) \
 	X(NtQueryDirectoryObject, NTSTATUS, (HANDLE, PVOID, ULONG, BOOLEAN, BOOLEAN, PULONG, PULONG)) \
 	X(NtQueryInformationProcess, NTSTATUS, (HANDLE, PROCESSINFOCLASS, PVOID, ULONG, PULONG)) \
-	X(NtReadVirtualMemory, NTSTATUS, (HANDLE, PVOID, PVOID, ULONG, PULONG))
+	X(NtQueryInformationToken, NTSTATUS, (HANDLE, TOKEN_INFORMATION_CLASS, PVOID, ULONG, PULONG)) \
+	X(NtQueryObject, NTSTATUS, (HANDLE, ULONG, PVOID, ULONG, PULONG)) \
+	X(NtQuerySecurityObject, NTSTATUS, (HANDLE, SECURITY_INFORMATION, PSECURITY_DESCRIPTOR, ULONG, PULONG)) \
+	X(NtQuerySymbolicLinkObject, NTSTATUS, (HANDLE, PUNICODE_STRING, PULONG)) \
+	X(NtQuerySystemInformation, NTSTATUS, (SYSTEM_INFORMATION_CLASS, PVOID, ULONG, PULONG)) \
+	X(NtReadVirtualMemory, NTSTATUS, (HANDLE, PVOID, PVOID, ULONG, PULONG)) \
+	X(NtSetInformationObject, NTSTATUS, (HANDLE, OBJECT_INFORMATION_CLASS, PVOID, ULONG)) \
+	X(NtSetInformationToken, NTSTATUS, (HANDLE, TOKEN_INFORMATION_CLASS, PVOID, ULONG)) \
+	X(NtSetSecurityObject, NTSTATUS, (HANDLE, SECURITY_INFORMATION, PSECURITY_DESCRIPTOR))
 
 #define X(name, ret, args) typedef ret (NTAPI *_##name##_t) args;
 NTDLL_OBJECT_EXPORTS
@@ -47,13 +62,12 @@ NTDLL_OBJECT_EXPORTS
 
 		static NtixObjectLib* ptr;
 
+		NTSTATUS query(HANDLE Handle, ULONG ObjectInformationClass, PVOID ObjectInformation, ULONG ObjectInformationLength, PULONG ReturnLength) const;
+		NTSTATUS query_directory(HANDLE hDir, PVOID buffer, ULONG bufferLength, BOOLEAN ReturnSingleEntry, BOOLEAN restartScan, PULONG pContext, PULONG pReturnLength) const;
+		NTSTATUS query_information_process(HANDLE ProcessHandle, PROCESSINFOCLASS ProcessInformationClass, PVOID ProcessInformation, ULONG ProcessInformationLength, PULONG ReturnLength) const;
+		NTSTATUS query_information_token(HANDLE TokenHandle, TOKEN_INFORMATION_CLASS TokenInformationClass, PVOID TokenInformation, ULONG TokenInformationLength, PULONG ReturnLength) const;
 		NTSTATUS query_symbolic_link(HANDLE hSym, PUNICODE_STRING usTarget, PULONG returnedLength) const;
-		NTSTATUS query_directory(HANDLE hDir, PVOID buffer, ULONG bufferLength, BOOLEAN ReturnSingleEntry,
-			BOOLEAN restartScan, PULONG pContext, PULONG pReturnLength) const;
-		NTSTATUS query_information_process(HANDLE ProcessHandle, PROCESSINFOCLASS ProcessInformationClass,
-			PVOID ProcessInformation, ULONG ProcessInformationLength, PULONG ReturnLength) const;
-		NTSTATUS read_virtual_memory(HANDLE ProcessHandle, PVOID BaseAddress,PVOID Buffer,
-			ULONG NumberOfBytesToRead,PULONG NumberOfBytesRead) const;
+		NTSTATUS read_virtual_memory(HANDLE ProcessHandle, PVOID BaseAddress,PVOID Buffer, ULONG NumberOfBytesToRead, PULONG NumberOfBytesRead) const;
 
 	public:
 		static NtixObjectLib* get_instance();
@@ -65,17 +79,41 @@ NTDLL_OBJECT_EXPORTS
 
 		// if it throws and doesn't return NTSTATUS, it's public
 
+		void adjust_privileges_token(HANDLE TokenHandle, PTOKEN_PRIVILEGES NewState, ULONG BufferLength) const;
+
 		void close(HANDLE h) const;
-		HANDLE open_symbolic_link(npath p, ACCESS_MASK am) const;
-		ULONG query_symbolic_link_size(HANDLE hSym) const;
+
+		void disable_privileges_token(HANDLE TokenHandle) const;
+
 		const npath get_symbolic_link_path(npath p) const;
-		HANDLE open_directory(npath p, ACCESS_MASK am) const;
-		std::vector<directory_info> read_directory(npath p) const;
 		const nstring get_type(npath p) const;
 		bool is_directory(npath p) const;
 		bool is_symlink(npath p) const;
 
+		void make_temporary(HANDLE h) const;
 
+		HANDLE open_directory(npath p, ACCESS_MASK am) const;
+		HANDLE open_event(npath p, ACCESS_MASK am) const;
+		HANDLE open_mutant(npath p, ACCESS_MASK am) const;
+		HANDLE open_process_token(HANDLE p, ACCESS_MASK am) const;
+		HANDLE open_section(npath p, ACCESS_MASK am) const;
+		HANDLE open_semaphore(npath p, ACCESS_MASK am) const;
+		HANDLE open_symbolic_link(npath p, ACCESS_MASK am) const;
+
+		BOOLEAN privilege_check(HANDLE p, PPRIVILEGE_SET ps) const;
+
+		ULONG query_size(HANDLE oh, ULONG oic) const;
+		ULONG query_information_process_size(HANDLE ph, PROCESSINFOCLASS pic) const;
+		ULONG query_information_token_size(HANDLE ph, TOKEN_INFORMATION_CLASS tic) const;
+		ULONG query_security_size(HANDLE Handle) const;
+		ULONG query_symbolic_link_size(HANDLE hSym) const;
+		ULONG query_system_information_size(SYSTEM_INFORMATION_CLASS SystemInformationClass) const;
+
+		std::vector<directory_info> read_directory(npath p) const;
+
+		void set_information(HANDLE Handle, OBJECT_INFORMATION_CLASS ObjectInformationClass, PVOID ObjectInformation, ULONG ObjectInformationLength) const;
+		void set_information_token(HANDLE TokenHandle, TOKEN_INFORMATION_CLASS TokenInformationClass, PVOID TokenInformation, ULONG TokenInformationLength) const;
+		void set_security(HANDLE Handle, SECURITY_INFORMATION SecurityInformation, PSECURITY_DESCRIPTOR SecurityDescriptor) const;
 	};
 }
 #endif
